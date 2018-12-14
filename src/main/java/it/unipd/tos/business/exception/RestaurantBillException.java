d3b37a24e487141a657f413af40a3ff53680f933
dinato97 <davide.dinato1997@gmail.com>
2018-12-14 09:50:04 +0100

    modifiche

--- src/main/java/it/unipd/tos/business/exception/RestaurantBillException.java
+++ src/main/java/it/unipd/tos/business/exception/RestaurantBillException.java
@@ -4,7 +4,6 @@
 
 package it.unipd.tos.business.exception;
 
-<<<<<<<HEAD
 
 public class RestaurantBillException extends Throwable {
 
@@ -16,18 +15,6 @@ public class RestaurantBillException extends Throwable {
     }
 
     public String getMessage() {
-=======
-        public class RestaurantBillException extends Throwable {
-            private String message;
-
-            public RestaurantBillException(String message) {
-                // TODO Auto-generated constructor stub
-                this.message = message;
-            }
-
-            public String getMessage() {
->>>>>>>5d 7ed 2 bbe3a7d4cb64eeb4d62a466e3bc5db8832
-                return message;
-            }
-
-        }
+        return message;
+    }
+}
