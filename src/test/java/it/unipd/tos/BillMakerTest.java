31ba39ea357de2654a311cb258d200499a7ac907
dinato97 <davide.dinato1997@gmail.com>
2018-12-14 09:21:58 +0100

    modifiche

--- src/test/java/it/unipd/tos/BillMakerTest.java
+++ src/test/java/it/unipd/tos/BillMakerTest.java
@@ -11,6 +11,7 @@ import it.unipd.tos.model.MenuItem;
 import it.unipd.tos.TestMenuItem;
 import it.unipd.tos.business.Bill;
 import java.util.ArrayList;
+import org.junit.rules.ExpectedException;
 
 public class BillMakerTest{
 	
@@ -27,14 +28,15 @@ public class BillMakerTest{
 			 conto.add(new MenuItem(MenuItem.itemType.PRIMO,"gnocchi ai 4 formaggi",4.5));
 			 
 			 Bill b = new Bill();
-			 
-			 assertEquals(b.getOrderPrice(conto), 31);
+             double tot = b.getOrderPrice(conto);
+             assertEquals(31, tot, 0.0);
 		 }
 		 catch(RestaurantBillException e){
 			 System.out.println(e.getMessage());
 		 }
 	 }
-	 
+
+	@org.junit.Test
 	 public void getOrderBillOver10Pizze() {
 		 try {
 			 List<MenuItem> conto = new ArrayList<MenuItem>();
@@ -51,14 +53,15 @@ public class BillMakerTest{
 			 conto.add(new MenuItem(MenuItem.itemType.PIZZA,"misto mare",11));
 			 
 			 Bill b = new Bill();
-			 
-			 assertEquals(b.getOrderPrice(conto), 65);
+             double tot = b.getOrderPrice(conto);
+             assertEquals(65, tot, 0.0);
 		 }
 		 catch(RestaurantBillException e){
 			 System.out.println(e.getMessage());
 		 }
 	 }
-	 
+
+	@org.junit.Test
 	 public void getOrderBillOver100Euro() {
 		 try {
 			 List<MenuItem> conto = new ArrayList<MenuItem>();
@@ -75,13 +78,52 @@ public class BillMakerTest{
 			 conto.add(new MenuItem(MenuItem.itemType.PRIMO,"pasta allo scoglio",15));
 			 
 			 Bill b = new Bill();
-			 
-			 assertEquals(b.getOrderPrice(conto), 123.025);
+             double tot = b.getOrderPrice(conto);
+			 assertEquals(129.5, tot, 0.0);
 		 }
 		 catch(RestaurantBillException e){
 			 System.out.println(e.getMessage());
 		 }
 	 }
-	
+
+	@org.junit.Rule
+	public ExpectedException exception = ExpectedException.none();
+
+	@org.junit.Test
+	public void getOrderBillOver20Element() {
+		try {
+			List<MenuItem> conto = new ArrayList<MenuItem>();
+
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"marinara",3.5));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"capricciosa",6));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"pugliese",6));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"capricciosa",6));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"misto funghi",7.5));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"patatine",6));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"margherita",4));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"4 formaggi",6.5));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"diavola",5.5));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"parmigiana",6.5));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"marinara",3.5));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"capricciosa",6));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"pugliese",6));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"capricciosa",6));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"misto funghi",7.5));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"patatine",6));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"margherita",4));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"margherita",4));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"4 formaggi",6.5));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"diavola",5.5));
+			conto.add(new MenuItem(MenuItem.itemType.PIZZA,"parmigiana",6.5));
+
+			Bill b = new Bill();
+			double tot = b.getOrderPrice(conto);
+			exception.expect(RestaurantBillException.class);
+			exception.expectMessage("+ di 20 elementi");
+		}
+		catch(RestaurantBillException e){
+			System.out.println(e.getMessage());
+		}
+	}
 	
 }
